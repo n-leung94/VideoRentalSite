0e8a815f4340b2fe7ee02f9bb8c531387cca0ffd
n-leung94 <56689279+n-leung94@users.noreply.github.com>
2020-06-18 21:24:15 +0800

    Initial Customers And Membership Types
    Added:
    Customer Controller Class and DB
    MembershipType Controller Class and DB

---
+++ VideoRentalService/VideoRentalService/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoRentalService.Models;
+using System.Data.Entity;
+
+namespace VideoRentalService.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // To initialise Database Object
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+
+        }
+
+        // Destructor for DB object
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();  // Fetches all customers in DB as list , Include Statement is needed as Membership Type is a different data object "Eager Loading"
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+    }
+}
--- VideoRentalService/VideoRentalService/Models/Customer.cs
+++ VideoRentalService/VideoRentalService/Models/Customer.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace VideoRentalService.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        // Required marks for the database migration that the name shouldn't be nullable when transforming it to table attributes. requires System.ComponentModel.DataAnnotations
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; } // Foreign Key to find the correct ID from Membership Type to apply to the customer when needed.
     }
 }
